53599c066758d573539e2b54c09a407e7c101d97
PavanKumar <51289314+Gumparthypavankumar@users.noreply.github.com>
2022-02-20 10:43:37 +0530

    fix

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,7 @@
-module.exports={
-    mongoURI:process.env.MONGO_URI,
-    googleClientId:process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET
-}
+module.exports = {
+  mongoURI:
+    "mongodb+srv://PavanKumar:aSGdj8FFIfZBBJYL@storybooks-prod.el2mo.mongodb.net/storybooks?retryWrites=true&w=majority",
+  googleClientId:
+    "1011115952426-3lv1qhlkndlsiahbkvols4a9olu0frl0.apps.googleusercontent.com",
+  googleClientSecret: "0JBpHko0SUoESM9h_M1e4wPn",
+};
